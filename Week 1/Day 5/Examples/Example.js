d9b672634e9d158bd86d80c4d5f71e3d2789be31
Adria <adrimadu19@gmail.com>
2022-07-22 16:42:46 +0200

    Example Done

--- Week 1/Day 5/Examples/Example.js
+++ Week 1/Day 5/Examples/Example.js
@@ -1,11 +1,22 @@
 
-const name = "Adrià";
-const fechaDeNacimiento = "19/04/2001";
-const lugarDeNacimiento = "Barcelona";
+// const name = "Adrià";
+// const fechaDeNacimiento = "19/04/2001";
+// const lugarDeNacimiento = "Barcelona";
 
-let age = [20, 19, 18];
+// let age = [20, 19, 18];
 
-console.log(fechaDeNacimiento);
+// console.log(fechaDeNacimiento);
 
+// 
 
+const drinkAge = parseInt(prompt("Introduce tu edad"));
+
+if (drinkAge <= 21) {
+
+    console.log(" No puedes comprar alcohol :decepcionado: ");
+
+}
+else {
+    console.log("Get drunk tonight baby :cara_ligeramente_sonriente:");
+}
 
