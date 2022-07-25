3d5d75aa400fae3683cee73d8730cf593f21249d
Adrià Madueño <adrimadu19@gmail.com>
2022-07-25 11:52:28 +0200

    Ex

---
+++ Week 2/Day 1/Examples/Ex1.js
@@ -0,0 +1,8 @@
+const name = "Adria"
+const secondName = "Madueño"
+let edad = "21"
+const hobby1 = "Taekwondo"
+const hobby2 = "Musica"
+const hobby3 = "Jugar Videojuegos"
+
+console.log('Hola me llamo ${name + secondName}, tengo ${edad} años y dentro de ${80-edad} años cumplire 80 años. Mis hobbies son, principalmente ${hobby1} y intento compaginarlo con ${hobby2} y ${hobby3}');
