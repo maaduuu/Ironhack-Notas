a7767cc97007e39d18d627872f40ac90c4a1e81c
Adria <adrimadu19@gmail.com>
2022-08-10 12:59:54 +0200

    Commit

---
+++ Week 4/Day2/examples/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div id="form-username-text-country">
+    <label for="name">Nom</label>
+    <input type="text" autocomplete="off" id="name" v-model="formValues.forName">
+    
+    </div>
+
+  </div>
+  
+  
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data(){
+    return {
+
+
+    }
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
